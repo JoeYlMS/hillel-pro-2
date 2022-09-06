bb152e4d8cb7dea200d13487f4650b7fc79893ec
JoeYlMS <ivbazulyk@gmail.com>
2022-09-06 16:12:12 +0300

    dz-27-

--- js/main.js
+++ js/main.js
@@ -1,19 +1,6 @@
 document.addEventListener('DOMContentLoaded', function (){
 
     let cont = document.querySelector('.cont')
-    let box = document.querySelectorAll('.box')
-    let next = document.querySelector('.next')
-    let number = document.querySelector('.number')
-    let prev = document.querySelector('.prev')
-
-
-
-    // box.forEach(function (box){
-    //     box.addEventListener('click', function (event){
-    //         if (event.target.classList.contains('next')) event.target.parentElement.querySelector('p').innerHTML++
-    //         if (event.target.classList.contains('prev')) event.target.parentElement.querySelector('p').innerHTML--
-    //     })
-    // })
     cont.addEventListener('click',(event)=>{
         if (event.target.classList.contains('next')) event.target.parentElement.querySelector('p').innerHTML++
         if (event.target.classList.contains('prev')) event.target.parentElement.querySelector('p').innerHTML--
